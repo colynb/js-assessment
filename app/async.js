ba897be84dcd44351606dd8306f707112bad7700
Colyn Brown <colyn.brown@gmail.com>
2012-08-06 20:52:58 -0700

    finished 'async' tests

--- app/async.js
+++ app/async.js
@@ -2,12 +2,42 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define([ 'jquery' ], function($) {
   return {
-    async : function(value) {
 
+  	/* 
+  	 * Thought process
+  	 *  1. This was a bit of a logic puzzle to make sure I followed the chain of functions correctly
+  	 *  2. Some trial and error but got it working.
+  	 */ 
+    async : function(value) {
+    	var my = {};
+    	my.then = function(fn) {
+    		fn(value);
+    	}
+    	return my;
     },
 
-    manipulateRemoteData : function(url) {
 
+    /*
+     * Thought process
+     *  1. This method takes a URL and returns an object with a then function
+	 *  2. 'Then' function instantiates ajax call and return a sorted array on success
+     */
+    manipulateRemoteData : function(url) {
+    	var my = {};
+    	my.then = function(fn) {
+    		$.ajax({
+	    		url: url,
+	    		success: function (result) {
+	    			var people = [];
+	    			for (var i = 0; i < result.people.length; i++) {
+	    				people.push(result.people[i].name);
+	    			}
+	    			people.sort();
+	    			fn(people);
+	    		}
+	    	});
+    	}
+    	return my;
     }
   };
 });
